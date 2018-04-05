a0d8b5c13b0cfde86ebbc85ef65d2a6a7bbdd11f
Kamimero <nothing@co.jp>
2018-04-06 06:22:36 +0900

    キャッシュ更新方式を変更

---
+++ cache_update.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+import os
+import distutils
+from distutils import dir_util
+homepath = os.path.expanduser('~')
+src = os.path.abspath(homepath + "/dotfiles/.cache")
+dst = os.path.abspath(homepath + "/" + ".vim/dein/.cache")
+distutils.dir_util.copy_tree(src, dst)

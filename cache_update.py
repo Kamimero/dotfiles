5e4407aafbf46de78019a3ec0ccaee40d96ea72f
Kamimero <nothing@co.jp>
2018-04-06 06:32:43 +0900

    キャッシュ更新方法変更

--- cache_update.py
+++ cache_update.py
@@ -3,6 +3,6 @@ import os
 import distutils
 from distutils import dir_util
 homepath = os.path.expanduser('~')
-src = os.path.abspath(homepath + "/dotfiles/.cache")
-dst = os.path.abspath(homepath + "/" + ".vim/dein/.cache")
+src = os.path.abspath(homepath + "/dotfiles/cache")
+dst = os.path.abspath(homepath + "/.vim/dein/.cache/.vimrc/.dein")
 distutils.dir_util.copy_tree(src, dst)
